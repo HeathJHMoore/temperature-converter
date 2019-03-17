79350ec8360d0b286379aed757132718d686494a
Heath JH Moore <heathjhmoore@gmail.com>
2019-03-17 18:10:08 -0500

    added toCelsius and toFahrenheit functions

--- project.js
+++ project.js
@@ -1,25 +1,43 @@
-console.log("yo whats up");
-
 const printToDom = (divID, textToPrint) => {
     const selectedDiv = document.getElementById(divID);
     selectedDiv.innerHTML = textToPrint;
 }
 
+const toCelsius = (input) => {
+    return newCelsius = (input * (9/5)) + 32;
+}
 
-const determineConverter = () => {
-    let tempInput = document.getElementById("tempInput").value;
-    let buttonInput = ''
+const toFahrenheit = (input) => {
+    return newFahrenheit = (input - 32) * (5/9);
+}
+
+
+const temperatureConverter = () => {
+    const tempInput = document.getElementById("tempInput").value;
     if (document.getElementById("celsiusButton").checked === true) {
-        buttonInput = 'C';
+        const newCelsius = `${toCelsius(tempInput)} C`;
+        printToDom("tempOutput", newCelsius);
     } else {
-        buttonInput = 'F';
-    }
-    console.log(tempInput, buttonInput);
-    printToDom('tempOutput', tempInput);
+        const newFahrenheit = `${toFahrenheit(tempInput)} F`;
+        printToDom("tempOutput", newFahrenheit);
+    };
+}
+
+const clear = () => {
+    document.getElementById("tempInput").value = ``;
+    document.getElementById("celsiusButton").checked = false;
+    document.getElementById("fahrenheitButton").checked = false;
+    document.getElementById("tempOutput").innerHTML = ``;
 }
 
 const buttonEvents = () => {
-    document.getElementById("convertButton").addEventListener("click", determineConverter);
+    document.getElementById("convertButton").addEventListener("click", temperatureConverter);
+    document.getElementById("clearButton").addEventListener("click", clear);
+    document.addEventListener("keyup", function(e) {
+        if (e.keyCode === 13) {
+            temperatureConverter();
+        };
+    });
 }
 
 const init = () => {
