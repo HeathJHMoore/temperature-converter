ee859cbe057cf38b5c325f838038606116eb6d4f
Heath JH Moore <heathjhmoore@gmail.com>
2019-03-17 17:30:27 -0500

    finished convert-button-events ticket

--- project.js
+++ project.js
@@ -1,1 +1,29 @@
 console.log("yo whats up");
+
+const printToDom = (divID, textToPrint) => {
+    const selectedDiv = document.getElementById(divID);
+    selectedDiv.innerHTML = textToPrint;
+}
+
+
+const determineConverter = () => {
+    let tempInput = document.getElementById("tempInput").value;
+    let buttonInput = ''
+    if (document.getElementById("celsiusButton").checked === true) {
+        buttonInput = 'C';
+    } else {
+        buttonInput = 'F';
+    }
+    console.log(tempInput, buttonInput);
+    printToDom('tempOutput', tempInput);
+}
+
+const buttonEvents = () => {
+    document.getElementById("convertButton").addEventListener("click", determineConverter);
+}
+
+const init = () => {
+    buttonEvents();
+}
+
+init();
